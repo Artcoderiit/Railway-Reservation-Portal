46be35a4b6ff18924177533641c1291cfb6bf684
unknown <2017meb1195@iitrpr.ac.in>
2020-11-26 02:33:48 +0530

    mybookings

--- app/my_bookings.py
+++ app/my_bookings.py
@@ -7,15 +7,14 @@ def my_bookings():
 		uname = req["uname"]
 
 		# adding the attributes that I need in AJAX
-		attr = ["trainno", "pnr", "source", "doj", "start_time", "dest", "end_time", "amount", "fname", "lname", "age", "gender", "coach", "seatno", "berth"]
-		tickets = db.execute("select * from passenger p1 where p1.pnr in (select t.pnr from ticket t where t.bookedby = (:uname))",
+		#attr = ["trainno", "pnr", "source", "doj", "start_time", "dest", "end_time", "amount", "fname", "lname", "age", "gender", "coach", "seatno", "berth"]
+		tickets = db.execute("select * from passenger p1,trains tr1 where p1.pnr in (select t.pnr from ticket t where t.bookedby = (:uname)) and tr1.trainno=p1.trainno and tr1.doj=p1.doj)",
 		{"uname":uname}).fetchall()
 
-		a = []
-		for i in tickets:
-			d = {}
-			for j in range(len(attr)):
-				d[attr[j]] = i[j]
+		d, a = {}, []
+		for rowproxy in tickets:
+			for column, value in rowproxy.items():
+				d = {**d, **{column: value}}
 			a.append(d)
 		
 		list_of_tickets = []
